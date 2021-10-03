4e31b58926f7098369ea61dad5a317d75d4da132
Baptiste-Taho <bapt.menot@gmail.com>
2021-10-03 12:12:34 +0200

    fix: use dict.get, remove white spaces, simplify code

--- Scripts/main.py
+++ Scripts/main.py
@@ -18,34 +18,33 @@ headers = {
 response = requests.get(url = url, headers = headers)
 response_list = json.loads(response.text)
 
-def write_options(options:list, spaces: int, file, command, recursive = False):
+def write_options(options:list, spaces:int, file, command, recursive:bool = False):
 
     def write_spaces(spaces:int):
-        for _i in range(spaces):
-            file.write(' ')
+        file.write(' '*spaces)
 
     if recursive is False:
         file.write('\n')
 
     for option in options:
 
-        option_name = option['name']
+        option_name = option.get('name')
         write_spaces(spaces)
         file.write(f'[{command}.{option_name}]\n\n')
 
-        option_type = option['type']
+        option_type = option.get('type')
         write_spaces(spaces)
         file.write(f'type = {option_type}\n')
 
         write_spaces(spaces)
         file.write(f'name = "{option_name}"\n')
 
-        option_description = option['description']
+        option_description = option.get('description')
         write_spaces(spaces)
         file.write(f'description = "{option_description}"\n')
 
-        if 'required' in option:
-            required = option['required']
+        required = option.get('required', None)
+        if required is not None:
             if required is True:
                 write_spaces(spaces)
                 file.write(f'required = true\n')
@@ -53,17 +52,17 @@ def write_options(options:list, spaces: int, file, command, recursive = False):
                 write_spaces(spaces)
                 file.write(f'required = false\n')
         
-        if 'choices' in option:
+        choices = option.get('choices', None)
+        if choices:
             file.write('\n')
-            choices = option['choices']
             for choice in choices:
-                choice_name = choice['name']
+                choice_name = choice.get('name')
                 write_spaces(spaces + 4)
                 file.write(f'[{option_name}.{choice_name}]\n\n')
                 write_spaces(spaces + 4)
                 file.write(f'name = "{choice_name}"\n')
 
-                choice_value = choice['value']
+                choice_value = choice.get('value')
                 write_spaces(spaces + 4)
                 if isinstance(choice_value, str):
                     file.write(f'value = "{choice_value}"\n')
@@ -75,11 +74,11 @@ def write_options(options:list, spaces: int, file, command, recursive = False):
 
         elif option_type in (1, 2):
             file.write('\n')
-            recursive_options = option['options']
+            recursive_options = option.get('options')
             write_options(recursive_options, spaces + 4, file, option_name, recursive = True)
         
         elif 'channel_types' in option:
-            channel_types = option['channel_types']
+            channel_types = option.get('channel_types')
             file.write(f'channel_types = {channel_types}\n')
 
         if option != options[-1]:   
@@ -89,16 +88,17 @@ def write_options(options:list, spaces: int, file, command, recursive = False):
 
 with open(f"./{config['other']['name']}.toml", 'w', encoding = 'utf-8') as file:
     for command in response_list:
-        name = command['name']
+
+        name = command.get('name')
         file.write(f'[{name}]\n\n')
 
-        command_id = command['id']
+        command_id = command.get('id')
         file.write(f'id = {command_id}\n')
 
-        command_type = command['type']
+        command_type = command.get('type')
         file.write(f'type = {command_type}\n')
 
-        application_id = command['application_id']
+        application_id = command.get('application_id')
         file.write(f'application_id = {application_id}\n')
 
         if 'guild_id' in command:
@@ -107,20 +107,20 @@ with open(f"./{config['other']['name']}.toml", 'w', encoding = 'utf-8') as file:
 
         file.write(f'name = "{name}"\n')
 
-        description = command['description']
+        description = command.get('description')
         file.write(f'description = "{description}"\n')
 
-        if 'options' in command:
-            options = command['options']
+        options = command.get('options', None)
+        if options:
             write_options(options, 4, file, name)
         
-        permission = command['default_permission']
+        permission = command.get('default_permission')
         if permission is True:
             file.write(f'permission = true\n')
         else:
             file.write(f'permission = false\n')
         
-        version = command['version']
+        version = command.get('version')
         file.write(f'version = {version}\n')
 
         file.write(f'\n\n')
