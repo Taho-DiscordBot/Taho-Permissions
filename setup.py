71c66327ed03617ff0040e2d618ae65097db8d9d
Baptiste <61520798+Baptiste-Taho@users.noreply.github.com>
2022-08-31 14:05:25 +0200

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Taho-Permissions",
-    version="0.0.1",
+    version="0.0.2",
     license="MIT",
     author="Baptiste",
     author_email="taho.discordbot@gmail.com",
